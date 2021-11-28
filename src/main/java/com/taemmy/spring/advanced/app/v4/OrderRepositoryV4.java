578f9852d89065843fd0264c2c0717978a56c05f
태영 <ttaeyoung@gmail.com>
2021-11-28 18:45:52 +0900

    section03: 템플릿 메서드 적용

---
+++ src/main/java/com/taemmy/spring/advanced/app/v4/OrderRepositoryV4.java
@@ -0,0 +1,37 @@
+package com.taemmy.spring.advanced.app.v4;
+
+import com.taemmy.spring.advanced.trace.TraceStatus;
+import com.taemmy.spring.advanced.trace.logtrace.LogTrace;
+import com.taemmy.spring.advanced.trace.template.AbstractTemplate;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderRepositoryV4 {
+
+    private final LogTrace trace;
+
+    public void save(String itemId) {
+        AbstractTemplate<Void> template = new AbstractTemplate<>(trace) {
+            @Override
+            protected Void call() {
+                if (itemId.equals("ex")) {
+                    throw new IllegalStateException("예외 발생!");
+                }
+
+                sleep(1_000);
+                return null;
+            }
+        };
+        template.execute("OrderRepository.save()");
+    }
+
+    private void sleep(int millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
