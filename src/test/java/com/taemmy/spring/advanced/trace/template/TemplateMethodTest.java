19d67d97758725ef225650f2a6fe0740ab3e879c
태영 <ttaeyoung@gmail.com>
2021-11-28 18:22:02 +0900

    section03: 템플릿 메서드 패턴 예제

--- src/test/java/com/taemmy/spring/advanced/trace/template/TemplateMethodTest.java
+++ src/test/java/com/taemmy/spring/advanced/trace/template/TemplateMethodTest.java
@@ -1,5 +1,8 @@
 package com.taemmy.spring.advanced.trace.template;
 
+import com.taemmy.spring.advanced.trace.template.code.AbstractTemplate;
+import com.taemmy.spring.advanced.trace.template.code.SubCClassLogic1;
+import com.taemmy.spring.advanced.trace.template.code.SubCClassLogic2;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
@@ -30,5 +33,37 @@ public class TemplateMethodTest {
         long endTime = System.currentTimeMillis();
         long resultTime = endTime - startTime;
         log.info("resultTime={}", resultTime);
+    }
+
+    /**
+     * 템플릿 메서드 패턴 적용
+     */
+    @Test
+    void templateMethodV1() {
+        AbstractTemplate template1 = new SubCClassLogic1();
+        template1.execute();
+
+        AbstractTemplate template2 = new SubCClassLogic2();
+        template2.execute();
+
+    }
+
+    @Test
+    void templateMethodV2() {
+        AbstractTemplate template1 = new AbstractTemplate() {
+            @Override
+            protected void call() {
+                log.info("비지니스 로직1 실행");
+            }
+        };
+        log.info("클래스 이름1={}", template1.getClass());
+
+        AbstractTemplate template2 = new AbstractTemplate() {
+            @Override
+            protected void call() {
+                log.info("비지니스 로직2 실행");
+            }
+        };
+        log.info("클래스 이름2={}", template2.getClass());
     }
 }
