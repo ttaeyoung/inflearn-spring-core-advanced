4bcca4595fd238c5aa4ca2942467f1a77e51819f
태영 <ttaeyoung@gmail.com>
2021-12-01 00:32:27 +0900

    section03: 템플릿 콜백 패턴 적용

---
+++ src/main/java/com/taemmy/spring/advanced/app/v5/OrderControllerV5.java
@@ -0,0 +1,33 @@
+package com.taemmy.spring.advanced.app.v5;
+
+import com.taemmy.spring.advanced.trace.callback.TraceCallback;
+import com.taemmy.spring.advanced.trace.callback.TraceTemplate;
+import com.taemmy.spring.advanced.trace.logtrace.LogTrace;
+import com.taemmy.spring.advanced.trace.template.AbstractTemplate;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class OrderControllerV5 {
+
+    private final OrderServiceV5 orderService;
+    private final TraceTemplate template;
+
+    public OrderControllerV5(OrderServiceV5 orderService, LogTrace trace) {
+        this.orderService = orderService;
+        this.template = new TraceTemplate(trace);
+    }
+
+    @GetMapping("/v5/request")
+    public String request(String itemId) {
+        return template.execute("OrderController.request()", new TraceCallback<>() {
+            @Override
+            public String call() {
+                orderService.orderItem(itemId);
+                return "ok";
+            }
+        });
+    }
+
+}
---
+++ src/main/java/com/taemmy/spring/advanced/trace/callback/TraceTemplate.java
@@ -0,0 +1,30 @@
+package com.taemmy.spring.advanced.trace.callback;
+
+import com.taemmy.spring.advanced.trace.TraceStatus;
+import com.taemmy.spring.advanced.trace.logtrace.LogTrace;
+
+public class TraceTemplate {
+
+    private final LogTrace trace;
+
+    public TraceTemplate(LogTrace trace) {
+        this.trace = trace;
+    }
+
+    public <T> T execute(String message, TraceCallback<T> callback) {
+        TraceStatus status = null;
+        try {
+            status = trace.begin(message);
+
+            // 로직 호출
+            T result = callback.call();
+
+            trace.end(status);
+            return result;
+        } catch (Exception e) {
+            trace.exception(status, e);
+            throw e;
+        }
+    }
+
+}
