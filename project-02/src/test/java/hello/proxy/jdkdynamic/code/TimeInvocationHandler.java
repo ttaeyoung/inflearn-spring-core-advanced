1209194a5028316b9170d19b62313475ea8e7eef
태영 <ttaeyoung@gmail.com>
2021-12-22 00:33:19 +0900

    section05: JDK 동적 프록시 예제 코드

---
+++ project-02/src/test/java/hello/proxy/jdkdynamic/code/TimeInvocationHandler.java
@@ -0,0 +1,28 @@
+package hello.proxy.jdkdynamic.code;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+
+@Slf4j
+public class TimeInvocationHandler implements InvocationHandler {
+
+    private final Object target;
+
+    public TimeInvocationHandler(Object target) {
+        this.target = target;
+    }
+
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        log.info("TimeProxy 실행");
+        long started = System.currentTimeMillis();
+
+        Object result = method.invoke(target, args);
+
+        long ended = System.currentTimeMillis();
+        log.info("TimeProxy 종료. result time: {}", (ended - started));
+        return result;
+    }
+}
