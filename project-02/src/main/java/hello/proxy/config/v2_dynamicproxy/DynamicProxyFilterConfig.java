5bb3844c55c176edee44ad577e37f57e12abf96c
태영 <ttaeyoung@gmail.com>
2021-12-22 20:56:59 +0900

    section05: JDK 동적 프록시 적용 2

--- project-02/src/main/java/hello/proxy/config/v2_dynamicproxy/DynamicProxyFilterConfig.java
+++ project-02/src/main/java/hello/proxy/config/v2_dynamicproxy/DynamicProxyFilterConfig.java
@@ -2,6 +2,7 @@ package hello.proxy.config.v2_dynamicproxy;
 
 import hello.proxy.app.v1.*;
 import hello.proxy.config.v2_dynamicproxy.handler.LogTraceBasicHandler;
+import hello.proxy.config.v2_dynamicproxy.handler.LogTraceFilterHandler;
 import hello.proxy.trace.logtrace.LogTrace;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,7 +10,9 @@ import org.springframework.context.annotation.Configuration;
 import java.lang.reflect.Proxy;
 
 @Configuration
-public class DynamicProxyBasicConfig {
+public class DynamicProxyFilterConfig {
+
+    private static final String[] PATTERNS = {"request*", "order*", "save*"};
 
     @Bean
     public OrderControllerV1 orderControllerV1(LogTrace logTrace) {
@@ -17,7 +20,7 @@ public class DynamicProxyBasicConfig {
 
         OrderControllerV1 proxy = (OrderControllerV1) Proxy.newProxyInstance(OrderControllerV1.class.getClassLoader(),
                 new Class[]{OrderControllerV1.class},
-                new LogTraceBasicHandler(target, logTrace));
+                new LogTraceFilterHandler(target, logTrace, PATTERNS));
         return proxy;
     }
 
@@ -40,4 +43,5 @@ public class DynamicProxyBasicConfig {
                 new LogTraceBasicHandler(target, logTrace));
         return proxy;
     }
+
 }
