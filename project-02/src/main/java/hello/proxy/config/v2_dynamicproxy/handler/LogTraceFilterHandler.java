0cc042bf5d52ce28976d448cf15704861c08b3b5
태영 <ttaeyoung@gmail.com>
2021-12-22 20:56:59 +0900

    section05: JDK 동적 프록시 적용 2

--- project-02/src/main/java/hello/proxy/config/v2_dynamicproxy/handler/LogTraceFilterHandler.java
+++ project-02/src/main/java/hello/proxy/config/v2_dynamicproxy/handler/LogTraceFilterHandler.java
@@ -2,24 +2,30 @@ package hello.proxy.config.v2_dynamicproxy.handler;
 
 import hello.proxy.trace.TraceStatus;
 import hello.proxy.trace.logtrace.LogTrace;
-import lombok.extern.slf4j.Slf4j;
+import org.springframework.util.PatternMatchUtils;
 
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 
-@Slf4j
-public class LogTraceBasichandler implements InvocationHandler {
+public class LogTraceFilterHandler implements InvocationHandler {
 
     private final Object target;
     private final LogTrace logTrace;
+    private final String[] patterns;
 
-    public LogTraceBasichandler(Object target, LogTrace logTrace) {
+    public LogTraceFilterHandler(Object target, LogTrace logTrace, String[] patterns) {
         this.target = target;
         this.logTrace = logTrace;
+        this.patterns = patterns;
     }
 
     @Override
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        String methodName = method.getName();
+        if (!PatternMatchUtils.simpleMatch(patterns, methodName)) {
+            return method.invoke(target, args);
+        }
+
         TraceStatus status = null;
         try {
             String message = String.format("%s.%s()",
